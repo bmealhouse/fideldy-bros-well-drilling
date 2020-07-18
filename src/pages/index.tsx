7f559f7f85239487f153a92fd4d890fc997cb7e0
Brent Mealhouse <bmealhouse@gmail.com>
2020-07-17 22:05:50 -0500

    Photo updates

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,9 +8,7 @@ import {
   Slogan,
 } from "../components";
 import well_drilling from "../assets/well-drilling.jpg";
-// import well_services_repair from "../assets/well-services-repair.jpg";
-import geothermal from "../assets/geothermal.jpg";
-import fideldy_bros from "../assets/fideldy-bros.jpg";
+import well_services_repair from "../assets/well-services-repair.jpg";
 import "typeface-inter";
 
 export default () => (
@@ -27,17 +25,10 @@ export default () => (
       />
       <HorizontalCTA
         position="right"
-        image={fideldy_bros}
+        image={well_services_repair}
         heading="Well services"
-        description="Permanently sealing an abandoned well protects groundwater quality and public health for future generations. We have the skills and equipment to seal abandoned wells. We also repair, sell, and service pumps! We are licensed to do well inspections and water testing."
+        description="Permanently sealing an abandoned well protects groundwater quality and public health for future generations. We have the skills and equipment to seal abandoned wells. We also repair, sell, and service pumps! We are licensed to do well inspections and water testing. Vertical geothermal installs are also available."
         buttonText="Contact us about well services"
-      />
-      <HorizontalCTA
-        position="left"
-        image={geothermal}
-        heading="Geothermal"
-        description="We realize that the proper sizing, design, and installation are critical to vertical geothermal heat exchange system performance and seasonal loop field recovery. Our training and experience provides your home with this environmentally friendly heating system."
-        buttonText="Contact us about geothermal"
       />
       <ContactUs />
     </Stack>
